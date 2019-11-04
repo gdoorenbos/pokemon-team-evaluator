0f2b86c44f3f3117a03684d23c244edfcfa06861
Garrett Doorenbos <garrett.doorenbos@adtran.com>
2019-11-04 16:17:33 -0600

    feat(team-evaluator): add types list

--- pokemon-team-evaluator.py
+++ pokemon-team-evaluator.py
@@ -1,3 +1,9 @@
+# pokemon-team-evaluator
+# type chart: https://img.rankedboost.com/wp-content/uploads/2018/10/Pokemon-Lets-Go-Type-Chart.jpg
+
+types = ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost',
+         'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon',
+         'dark', 'fairy']
 
 # if a multiplier is not given, it is assumed to be 1
 type_strengths = {
